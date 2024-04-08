fef364a65c27151fb45d4749fed81da5ce10eba1
Guilherme-Pinto-Almeida <109819841+GuilhermePint@users.noreply.github.com>
2024-04-07 22:02:34 -0300

    estilização css

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 
 
+var iniciarJogo = document.getElementById("initGame");
+iniciarJogo.addEventListener("click", initGame);
+
+var mainPage = document.getElementById("mainPage");
+var resetButton = document.getElementById("resetButton");
+
+async function initGame() {
+    createGameInstance("game1");
+    createGameInstance("game2");
+    mainPage.classList.remove("hidden");
+    mainPage.classList.add("page");
+
+    resetButton.classList.remove("hidden");
+    resetButton.classList.add("button-6");
+    resetButton.classList.add("animat");
+
+    iniciarJogo.classList.remove("bitt")
+    iniciarJogo.classList.add("hidden")
+
+}
+
